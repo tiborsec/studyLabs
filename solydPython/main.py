e6c42896e869f10e39b1b53a16bc6aba5ca5cd8f
7ib0rNote <7ib0r@protonmail.com>
2021-12-22 15:31:01 -0300

    add python examples

---
+++ solydPython/main.py
@@ -0,0 +1,12 @@
+nome = ''
+sobrenome = 'Assenheimer'
+idade = ''
+
+
+print 'Qual e o seu nome?'
+nome = raw_input()
+
+print 'Qual e a sua idade?'
+idade = raw_input()
+
+print 'Nome: ' + nome + ' ' + sobrenome + ' e tenho ' + str(idade) + ' anos.'
